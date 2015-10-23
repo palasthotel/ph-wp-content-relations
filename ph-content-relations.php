b2b393151be085dda8d399be4c3b97a776db0263
Edward <edward.bock@palasthotel.de>
2015-10-23 12:53:06 +0200

    public functions #1

--- ph-content-relations.php
+++ ph-content-relations.php
@@ -52,9 +52,44 @@ function run_ph_content_relations()
 run_ph_content_relations();
 
 /**
+ * Adds a new relation
+ * @param $post_id_source Post ID
+ * @param $post_id_target Post ID
+ * @param $relation_type type string
+ * @return false|int|void
+ */
+function ph_content_realtions_add_realtion($post_id_source, $post_id_target, $relation_type){
+	$store = new Content_Relations_Store($post_id_source);
+	return $store->add_relation($post_id_target, $relation_type);
+}
+
+/**
+ * All relations by post id
+ * @param $post_id
+ * @return relations
+ */
+function ph_content_realtions_get_realtions_by_post_id($post_id){
+	$store = new Content_Relations_Store($post_id);
+	return $store->get_relations();
+}
+
+/**
+ * All relations by post id and type. Optional you can get both directions and not only relations where post is source
+ * @param $post_id
+ * @param $relation_type
+ * @param bool|true $source_only
+ * @return array
+ */
+function ph_content_realtions_get_realtions_by_post_id_and_type($post_id, $relation_type, $source_only = true){
+	$store = new Content_Relations_Store($post_id);
+	return $store->get_relations_by_type($relation_type, $source_only);
+}
+
+
+/**
  * -------------> PH Migrate Part
  */
-	/**
+/**
  * registeres handler for content relations
  *
  */
