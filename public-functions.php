74b93c6203deab505fcb7ea3973232ff658dc9eb
EdwardBock <edward.bock@palasthotel.de>
2018-10-22 15:02:16 +0200

    deprecated with ph_ prefix

--- public-functions.php
+++ public-functions.php
@@ -7,9 +7,21 @@
  * @param $relation_type string type
  * @return false|int
  */
-function ph_content_relations_add_relation($post_id_source, $post_id_target, $relation_type){
+function content_relations_add_relation($post_id_source, $post_id_target, $relation_type){
 	$store = new Content_Relations_Store($post_id_source);
-	return $store->add_relation($post_id_target, $relation_type);
+	return $store->add_relation($post_id_source, $post_id_target, $relation_type);
+}
+
+/**
+ * @param $post_id_source
+ * @param $post_id_target
+ * @param $relation_type
+ *
+ * @return false|int
+ * @deprecated will be remove with version 2
+ */
+function ph_content_relations_add_relation($post_id_source, $post_id_target, $relation_type){
+	return content_relations_add_relation($post_id_source, $post_id_target, $relation_type);
 }
 
 /**
@@ -17,9 +29,19 @@ function ph_content_relations_add_relation($post_id_source, $post_id_target, $re
  * @param $post_id
  * @return array relations
  */
-function ph_content_relations_get_relations_by_post_id($post_id){
+function content_relations_get_relations_by_post_id($post_id){
 	$store = new Content_Relations_Store($post_id);
 	return $store->get_relations();
+}
+
+/**
+ * @param $post_id
+ *
+ * @return array
+ * @deprecated will be removed with version 2
+ */
+function ph_content_relations_get_relations_by_post_id($post_id){
+	return content_relations_get_relations_by_post_id($post_id);
 }
 
 /**
@@ -29,7 +51,19 @@ function ph_content_relations_get_relations_by_post_id($post_id){
  * @param bool|true $source_only
  * @return array
  */
-function ph_content_relations_get_relations_by_post_id_and_type($post_id, $relation_type, $source_only = true){
+function content_relations_get_relations_by_post_id_and_type($post_id, $relation_type, $source_only = true){
 	$store = new Content_Relations_Store($post_id);
 	return $store->get_relations_by_type($relation_type, $source_only);
+}
+
+/**
+ * @param $post_id
+ * @param $relation_type
+ * @param bool $source_only
+ *
+ * @return array
+ * @deprecated will be removed with version 2
+ */
+function ph_content_relations_get_relations_by_post_id_and_type($post_id, $relation_type, $source_only = true){
+	return content_relations_get_relations_by_post_id_and_type($post_id, $relation_type, $source_only);
 }
